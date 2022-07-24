b5bb0a1cf2dc2b060abd84c1727f8cf284810951
hasegawa <th077lily2021@gmail.com>
2022-07-24 06:02:42 +0000

    laravel16 update

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::group(['prefix' => 'admin'], function(){
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
     
     // profile
     // admin/profile/create にアクセスしたら ProfileController の add Action
